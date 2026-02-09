9a734ee970b498677095d408134a2ad93c1f7988
Jenny <jenny@circleci.com>
2026-02-09 16:09:45 +0000

    update maths

--- myproj/lib/math.py
+++ myproj/lib/math.py
@@ -9,6 +9,9 @@ def subtract(a: int, b: int) -> int:
 def multiply(a: int, b: int) -> int:
     return a * b
 
+def multiply_again(a: int, b: int) -> int:
+    return a * b
+
 
 def divide(a: int, b: int) -> float:
     if b == 0:
